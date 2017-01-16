3fbae0372d66cb3a6bf01d2ab7e176e63e477ad7
Sachin Maharjan <sachin.gen@gmail.com>
2017-01-16 14:26:56 -0800

    First commit for chinstagram

---
+++ client/components/Single.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router';
+
+class Single extends Component {
+
+  render() {
+    return (
+      <div className="single-grid">
+        Single
+      </div>
+    )
+  }
+}
+
+export default Single;
